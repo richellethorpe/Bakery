78971a3f1ee52c52dc907118e28d3e66fa0a8716
Richelle <richelle.ak@gmail.com>
2023-02-11 09:44:41 -0800

    add good fail second test getBreadQty

--- Bakery.Tests/ModelTests/PricingTests.cs
+++ Bakery.Tests/ModelTests/PricingTests.cs
@@ -10,9 +10,18 @@ namespace Pricing.Tests
     [TestMethod]
     public void GlutenConstructor_CreateInstance_Gluten()
     {
-      Gluten newGluten = new Gluten();
+      Gluten newGluten = new Gluten(6, 7);
       Assert.AreEqual(typeof(Gluten),newGluten.GetType());
     }
 
+    [TestMethod]
+    public void GetBreadQty_ReturnBreadQty_Int()
+    {
+      int breadQty = 4;
+      int pastryQty = 5;
+      Gluten newGluten = new Gluten(breadQty, pastryQty);
+      string result = newGluten.BreadQty;
+      Assert.AreEqual(breadQty, result);
+    }
   }
 }
