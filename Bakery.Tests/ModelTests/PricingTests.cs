33746cfd8b44298f7b56624cacbe17c416e92e3e
Richelle <richelle.ak@gmail.com>
2023-02-11 09:02:30 -0800

    add pricigTests file

---
+++ Bakery.Tests/ModelTests/PricingTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Game.Models;
+using System;
+
+namespace Pricing.Tests
+{
+  [TestClass]
+  public class PricingTests
+  {
+    [TestMethod]
+    
+  }
+}
---
+++ Bakery/Models/Pricing.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Pricing.Models
+{
+  public class Gluten
+  {
+    public string Qty { get; set; }
+    public Gluten (int Qty)
+    {
+      Qty = qty;
+    }
+  }
+}
